3019a2912432c677ba2186a4dac434d9dd9c6541
Justine Lo <justine@ecotree.fr>
2023-04-14 21:01:27 +0200

    handle edit function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,38 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
+import { setProfile } from "../state/store";
 
 function Header() {
     const profile = useSelector((state: any) => state.auth.profile);
+    const dispatch = useDispatch();
     const [edit, setEdit] = useState(false);
     const [firstName, setFirstName] = useState(profile.firstName);
     const [lastName, setLastName] = useState(profile.lastName);
 
+    function handleEditButton() {
+        setEdit(!edit);
+        if (edit) {
+            dispatch(setProfile({ ...profile, firstName, lastName }));
+        }
+    }
+
     return (
         <div className="header">
           <h1>Welcome back</h1>
-          {edit ? <h1>{`${profile.firstName} ${profile.lastName}!`}</h1> :
-            <div>
+          {edit ? <div>
                <input onChange={(e) => setFirstName(e.target.value)} placeholder={profile.firstName}></input>
                <input onChange={(e) => setLastName(e.target.value)} placeholder={profile.lastName}></input>
-            </div>
+            </div> :
+            <h1>{`${profile.firstName} ${profile.lastName}!`}</h1>
             }
         
-          <button className="edit-button" onClick={() => setEdit(!edit)}>
+          <button className="edit-button" onClick={handleEditButton}>
             {edit ? 'Save' : 'Edit Name'}
           </button>
+          {edit && <button className="edit-button" onClick={handleEditButton}>
+            Cancel
+          </button> }
+          
         </div>
     );
 }
