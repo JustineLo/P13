06f68ab35f863a99d3b7b757d0c5dcee2ffd79af
Justine Lo <justine@ecotree.fr>
2023-05-19 13:55:58 +0200

    create account component

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -6,11 +6,13 @@ import { AppState, setProfile, signOut } from '../state/store';
 import { useEffect } from 'react';
 import { getProfile } from '../api';
 import Header from '../components/Header';
+import Account from '../components/Account';
 
 function Profile() {
   const profile = useSelector((state: AppState) => state.auth.profile);
   const storedToken = localStorage.getItem('token') || '';
   const dispatch = useDispatch();
+  
 
   useEffect(() => {
     async function fetchProfile() {
@@ -22,7 +24,6 @@ function Profile() {
       }
     }
     fetchProfile();
-  
   }, [storedToken, dispatch]);
   
 
@@ -62,36 +63,9 @@ function Profile() {
       <main className="main bg-dark">
         <Header />
         <h2 className="sr-only">Accounts</h2>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-            <p className="account-amount">$10,928.42</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-            <p className="account-amount">$184.30</p>
-            <p className="account-amount-description">Current Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
+        <Account title='Argent Bank Checking (x8349)' amount='$2,082.79' description='Available Balance' />
+        <Account title="Argent Bank Savings (x6712)" amount="$10,928.42" description="Available Balance" />
+        <Account title="Argent Bank Credit Card (x8349)" amount="$184.30" description="Current Balance" />
       </main>
       <footer className="footer">
         <p className="footer-text">Copyright 2020 Argent Bank</p>
